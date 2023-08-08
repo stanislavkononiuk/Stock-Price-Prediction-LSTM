c750e9a9bac1022dd2545f4e44c7ac1313319a02
stanislavkononiuk <stanislavkononiuk@gmail.com>
2023-08-08 17:02:19 +0800

    done

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,8 @@ def new_dataset(dataset, step_size):
 	data_X, data_Y = [], []
 	for i in range(len(dataset)-step_size-1):
 		a = dataset[i:(i+step_size), 0]
-		
+		data_X.append(a)
+		data_Y.append(dataset[i + step_size, 0])
 	return np.array(data_X), np.array(data_Y)
 
 # THIS FUNCTION CAN BE USED TO CREATE A TIME SERIES DATASET FROM ANY 1D ARRAY	
